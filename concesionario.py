264330cb16006a9fdf2796e65e3d866f763da493
Periz4 <180030@iessanalberto.com>
2024-11-20 10:17:15 +0100

    muchos cambios

--- concesionario.py
+++ concesionario.py
@@ -9,9 +9,26 @@ class Coche:
         self.traccion=traccion
         self.cilindrada=cilindrada
 
+cont=1
 
-cohce1=Coche("daciaDuster", 135, 19000, 240, "delantera", "I3")
+def siguienteCoche():
+    lblModeloData.config(text=listaCoches[cont].modelo)
+    lblcvsData.config(text=listaCoches[cont].cvs)
+    lblPrecioData.config(text=listaCoches[cont].precio)
+    lblparMotorData.config(text=listaCoches[cont].parMotor)
+    lblTraccionData.config(text=listaCoches[cont].traccion)
+    lblCilindradaData.config(text=listaCoches[cont].cilindrada)
+    cont=cont+1
 
+
+
+coche1=Coche("daciaDuster", 135, 19000, 240, "Delantera", "I3")
+coche2=Coche("rangeRoverSV", 635, 237000, 800, "Total", "V8")
+coche3=Coche("mercedesAMGGt63S", 639, 228443, 900, "Total", "V8")
+coche4=Coche("renaultClio", 101, 17300, 170, "Delantera", "I3")
+coche5=Coche("cupraFormentor", 333, 56090, 420, "Total", "I4")
+
+listaCoches=[coche1, coche2, coche3, coche4, coche5]
 
 window = tk.Tk()
 window.title("Mi concesionario")
@@ -20,12 +37,41 @@ window.geometry("400x300")
 lblTitulo=tk.Label(window, text="Concesionario San Alberto")
 lblTitulo.pack()
 
-class Coche:
-    modelo=str
+frmInfoCoches=tk.Frame(window)
+frmInfoCoches.pack()
 
+lblModelo=tk.Label(frmInfoCoches, text="Modelo: ")
+lblModelo.grid(column="1", row="1")
+lblModeloData=tk.Label(frmInfoCoches, text=listaCoches[0].modelo)
+lblModeloData.grid(column="2", row="1")
 
+lblcvs=tk.Label(frmInfoCoches, text="Caballos: ")
+lblcvs.grid(column="1", row="2")
+lblcvsData=tk.Label(frmInfoCoches, text=listaCoches[0].cvs)
+lblcvsData.grid(column="2", row="2")
 
+lblprecio=tk.Label(frmInfoCoches, text="Precio: ")
+lblprecio.grid(column="1", row="3")
+lblPrecioData=tk.Label(frmInfoCoches, text=listaCoches[0].precio)
+lblPrecioData.grid(column="2", row="3")
 
+lblparMotor=tk.Label(frmInfoCoches, text="Par Motor: ")
+lblparMotor.grid(column="1", row="4")
+lblparMotorData=tk.Label(frmInfoCoches, text=listaCoches[0].parMotor)
+lblparMotorData.grid(column="2", row="4")
+
+lblTraccion=tk.Label(frmInfoCoches, text="Tracción: ")
+lblTraccion.grid(column="1", row="5")
+lblTraccionData=tk.Label(frmInfoCoches, text=listaCoches[0].traccion)
+lblTraccionData.grid(column="2", row="5")
+
+lblCilindrada=tk.Label(frmInfoCoches, text="Cilindrada: ")
+lblCilindrada.grid(column="1", row="6")
+lblCilindradaData=tk.Label(frmInfoCoches, text=listaCoches[0].cilindrada)
+lblCilindradaData.grid(column="2", row="6")
+
+btnSiguenteCoche=tk.Button(window, text=("Siguiente"), command=siguienteCoche)
+btnSiguenteCoche.pack()
 
 
 
