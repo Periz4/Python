05994fa001e119470d5c8f3674d15381b42841aa
Periz4 <180030@iessanalberto.com>
2024-11-25 11:20:26 +0100

    kms

--- concesionario.py
+++ concesionario.py
@@ -8,17 +8,28 @@ class Coche:
         self.parMotor=parMotor
         self.traccion=traccion
         self.cilindrada=cilindrada
+        self.kilometros=0
 
-cont=1
+        def actualizarkms(self, kms):
+            self.kilometros = self.kilometros + kms
+
+    global cont
 
 def siguienteCoche():
-    lblModeloData.config(text=listaCoches[cont].modelo)
-    lblcvsData.config(text=listaCoches[cont].cvs)
-    lblPrecioData.config(text=listaCoches[cont].precio)
-    lblparMotorData.config(text=listaCoches[cont].parMotor)
-    lblTraccionData.config(text=listaCoches[cont].traccion)
-    lblCilindradaData.config(text=listaCoches[cont].cilindrada)
-    cont=cont+1
+
+    if Coche.cont < len(listaCoches)-1:
+        Coche.cont=Coche.cont + 1
+    else:
+        Coche.cont=0
+
+    lblModeloData.config(text=listaCoches[Coche.cont].modelo)
+    lblcvsData.config(text=listaCoches[Coche.cont].cvs)
+    lblPrecioData.config(text=listaCoches[Coche.cont].precio)
+    lblparMotorData.config(text=listaCoches[Coche.cont].parMotor)
+    lblTraccionData.config(text=listaCoches[Coche.cont].traccion)
+    lblCilindradaData.config(text=listaCoches[Coche.cont].cilindrada)
+    
+    
 
 
 
@@ -27,6 +38,7 @@ coche2=Coche("rangeRoverSV", 635, 237000, 800, "Total", "V8")
 coche3=Coche("mercedesAMGGt63S", 639, 228443, 900, "Total", "V8")
 coche4=Coche("renaultClio", 101, 17300, 170, "Delantera", "I3")
 coche5=Coche("cupraFormentor", 333, 56090, 420, "Total", "I4")
+Coche.cont=0
 
 listaCoches=[coche1, coche2, coche3, coche4, coche5]
 
@@ -69,6 +81,9 @@ lblCilindrada=tk.Label(frmInfoCoches, text="Cilindrada: ")
 lblCilindrada.grid(column="1", row="6")
 lblCilindradaData=tk.Label(frmInfoCoches, text=listaCoches[0].cilindrada)
 lblCilindradaData.grid(column="2", row="6")
+
+lblKilometros=tk.Label(frmInfoCoches, text="Kiómetros")
+lblKilometros.grid(column="1")
 
 btnSiguenteCoche=tk.Button(window, text=("Siguiente"), command=siguienteCoche)
 btnSiguenteCoche.pack()
